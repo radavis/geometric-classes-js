dff8e9a63499b1928c46043a000e8723c8e73995
Richard Davis <rdavis.bacs@gmail.com>
2015-09-30 10:24:52 -0400

    example without using prototype

---
+++ src/OtherSquare.js
@@ -0,0 +1,32 @@
+// without using prototype
+
+function newSquare(length, x, y) {
+  return {
+    length: length,
+    width: length,
+    x: x || 0,
+    y: y || 0,
+
+  diameter: function() {
+    var length_squared = Math.pow(this.length, 2);
+    var width_squared = Math.pow(this.width, 2);
+    return Math.sqrt(length_squared + width_squared);
+  },
+
+  perimeter: function() {
+    return this.length * 4;
+  },
+
+  area: function() {
+    return Math.pow(this.length, 2);
+  },
+
+  containsPoint: function() {
+    var delta = this.width / 2;
+    if(((this.x - delta <= x) && (x <= this.x + delta)) &&
+      ((this.y - delta <= y) && (y <= this.y + delta))) {
+      return true;
+    }
+    return false;
+  }
+}
