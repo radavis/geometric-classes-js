32fe26f85afa542ef905f4534b69b78ca01ef449
Richard Davis <rdavis.bacs@gmail.com>
2015-09-30 10:32:03 -0400

    make tests pass

--- src/OtherSquare.js
+++ src/OtherSquare.js
@@ -1,5 +1,4 @@
 // without using prototype
-
 function newSquare(length, x, y) {
   return {
     length: length,
@@ -7,26 +6,27 @@ function newSquare(length, x, y) {
     x: x || 0,
     y: y || 0,
 
-  diameter: function() {
-    var length_squared = Math.pow(this.length, 2);
-    var width_squared = Math.pow(this.width, 2);
-    return Math.sqrt(length_squared + width_squared);
-  },
+    diameter: function() {
+      var length_squared = Math.pow(this.length, 2);
+      var width_squared = Math.pow(this.width, 2);
+      return Math.sqrt(length_squared + width_squared);
+    },
 
-  perimeter: function() {
-    return this.length * 4;
-  },
+    perimeter: function() {
+      return this.length * 4;
+    },
 
-  area: function() {
-    return Math.pow(this.length, 2);
-  },
+    area: function() {
+      return Math.pow(this.length, 2);
+    },
 
-  containsPoint: function() {
-    var delta = this.width / 2;
-    if(((this.x - delta <= x) && (x <= this.x + delta)) &&
-      ((this.y - delta <= y) && (y <= this.y + delta))) {
-      return true;
+    containsPoint: function(x, y) {
+      var delta = this.width / 2;
+      if(((this.x - delta <= x) && (x <= this.x + delta)) &&
+        ((this.y - delta <= y) && (y <= this.y + delta))) {
+        return true;
+      }
+      return false;
     }
-    return false;
   }
 }
